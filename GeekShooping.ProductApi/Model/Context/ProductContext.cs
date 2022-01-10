fb3145ba32da78a1d087384b3e821f6a5d0b0228
Robson de Moraes Silva <robson.m.silva@animaeducacao.com.br>
2022-01-10 15:20:55 -0300

    Initial commit

---
+++ GeekShooping.ProductApi/Model/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.ProductApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShooping.ProductApi.Model.Context
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() { }
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
