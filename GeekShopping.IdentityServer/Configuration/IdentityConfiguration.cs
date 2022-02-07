3e8482d005bc11e1bcfcc42db527d1e04dc58c0c
Robson de Moraes Silva <rms2307@outlook.com>
2022-02-07 19:22:53 -0300

    Update IdentityConfiguration.cs

--- GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 using System.Collections.Generic;
 
 namespace GeekShopping.IdentityServer.Configuration
@@ -24,5 +25,34 @@ namespace GeekShopping.IdentityServer.Configuration
                 new ApiScope(name: "write", "Write data"),
                 new ApiScope(name: "delete", "Delete data")
             };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = {new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowedScopes = { "read", "write", "profile"}
+                },
+                new Client
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets = {new Secret("my_super_secret".Sha256())},
+                    RedirectUris= { "http://localhost:38773/signin-oidc" },
+                    PostLogoutRedirectUris = {"http://localhost:38773/signout-callback-oidc"},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
+                }
+            };
+
+
     }
 }
