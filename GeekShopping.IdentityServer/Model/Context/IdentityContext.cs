86c7b8a6fc0d0efc9f332b17560749b6d3aea5d7
Robson de Moraes Silva <rms2307@outlook.com>
2022-02-07 19:22:30 -0300

    Add: Projeto IdentityServer

---
+++ GeekShopping.IdentityServer/Model/Context/IdentityContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+    }
+}
